824ac2599f8ae7e39dd9efcaa4b6e16298d2eb4f
Ted Wei <jdwei@hotmail.com>
2016-12-25 21:22:53 +0800

    set this new directive up in application’s @NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ElasticHeader } from '../components/elastic-header/elastic-header';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ElasticHeader
   ],
   imports: [
     IonicModule.forRoot(MyApp)
